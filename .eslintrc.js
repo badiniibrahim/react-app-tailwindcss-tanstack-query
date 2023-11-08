bfc0b3563565a8a626293784746ea15ff2f1a2ed
Ibrahimovic <ibrahimovic@MacBook-Air-de-Ibrahimovic.local>
2023-11-08 12:05:48 +0100

    fix build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,41 @@
 module.exports = {
-    extends: ['react-app', 'react-app/jest', 'plugin:storybook/recommended'],
-    //... existing
-    plugins: ['import'],
-    rules: {
-      // this is for sorting WITHIN an import
-      'sort-imports': [
-        'error',
-        { ignoreCase: true, ignoreDeclarationSort: true },
-      ],
-      // this is for sorting imports
-      'import/order': [
-        'error',
-        {
-          groups: [
-            ['external', 'builtin'],
-            'internal',
-            ['sibling', 'parent'],
-            'index',
-          ],
-          pathGroups: [
-            {
-              pattern: '@(react|react-native)',
-              group: 'external',
-              position: 'before',
-            },
-            {
-              pattern: '@src/**',
-              group: 'internal',
-            },
-          ],
-          pathGroupsExcludedImportTypes: ['internal', 'react'],
-          'newlines-between': 'always',
-          alphabetize: {
-            order: 'asc',
-            caseInsensitive: true,
+  extends: ['react-app', 'react-app/jest', 'plugin:storybook/recommended'],
+  //... existing
+  plugins: [],
+  rules: {
+    // this is for sorting WITHIN an import
+    'sort-imports': [
+      'error',
+      { ignoreCase: true, ignoreDeclarationSort: true },
+    ],
+    // this is for sorting imports
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['external', 'builtin'],
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: '@(react|react-native)',
+            group: 'external',
+            position: 'before',
           },
+          {
+            pattern: '@src/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['internal', 'react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
         },
-      ],
-    },
-  };
+      },
+    ],
+  },
+};
--- README.md
+++ README.md
@@ -19,12 +19,15 @@ The repository is dedicated to Tanstack Query a simple and effective solution to
 ![GitHub last commit](https://img.shields.io/github/last-commit/badiniibrahim/react-app-tailwindcss-tanstack-query)
 ![Github Repo Size](https://img.shields.io/github/repo-size/badiniibrahim/react-app-tailwindcss-tanstack-query)
 
+### [LiveSite](https://react-redux-rtk-saga-tailwindcss-typescript.vercel.app/)
+
 ## Technologies
 
 - React.js
 - Beautiful layouts with TailwindCSS
 - Tanstack Query
 - Storybook
+- React testing library
 
 ## 👨‍💻 Getting Started
 
